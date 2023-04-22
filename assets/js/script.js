caea24e93822ad83f6ca742beb622e1a7e988d91
Humberto Martinez <humberto.martinez.matos@gmail.com>
2023-04-22 19:48:59 -0400

    Se agrega sección de destacados y formulario de contacto. Se importa el script del JS y JQuery

---
+++ assets/js/script.js
@@ -0,0 +1,3 @@
+// HABILITAR TOOLTIP
+const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
+const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
