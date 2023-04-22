6a47e8388bbb6b1c799d57c48e4efae169d51cc8
Humberto Martinez <humberto.martinez.matos@gmail.com>
2023-04-22 19:57:19 -0400

    Se agrega footer y Smooth Scroll

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,18 @@
 // HABILITAR TOOLTIP
 const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
 const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
+
+//SMOOTH SCROLL
+$(document).ready(function(){
+    $("a").on('click', function(event) {
+        if (this.hash !== "") {
+            event.preventDefault();
+            var hash = this.hash;
+            $('html, body').animate({
+                scrollTop: $(hash).offset().top
+            }, 1000, function(){
+                window.location.hash = hash;
+            });
+        } 
+    });
+});
